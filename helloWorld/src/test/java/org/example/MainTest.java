ae96a846edf0e89f9a17a3b470e1e1796192cebf
rmagdelijns <r.magdelijns@gmail.com>
2022-11-03 13:44:06 +0100

    hello world

--- helloWorld/src/test/java/org/example/MainTest.java
+++ helloWorld/src/test/java/org/example/MainTest.java
@@ -1,4 +1,32 @@
+package org.example;
+
+import junit.framework.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MainTest {
-  
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    void testIfPrintIsHelloWorld() {
+        Main.main(null);
+        assertEquals("hello world" + System.lineSeparator(), outContent.toString());
+    }
 }
